ddba25f906a21399dc4ac5c624a19e163acb0f96
andris1177 <laszloandris1@gmail.com>
2024-08-28 16:08:32 +0200

    update brightness.sh to round up the percentage

--- scripts/brightness.sh
+++ scripts/brightness.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 brightness=$(brightnessctl g)
 max_brightness=$(brightnessctl m)
-brightness_percent=$(( 100 * brightness / max_brightness ))
-echo "Brightness: ${brightness_percent}%"
+brightness_percent=$(awk -v b="$brightness" -v m="$max_brightness" 'BEGIN { print (100 * b) / m }')
+brightness_rounded=$(awk -v bp="$brightness_percent" 'BEGIN { print int(bp+0.5) }')
+echo "Brightness: ${brightness_rounded}%"

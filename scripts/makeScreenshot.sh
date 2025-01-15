b4fb1c43d3406b209e42e2b43a5cf063a8671212
andris1177 <laszloandris1@gmail.com>
2025-01-15 19:56:47 +0100

    remove files

--- scripts/makeScreenshot.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sleep 0.1
-screenshot_dir="/data/photos-videos/screenshots"
-filename="$screenshot_dir/screenshot_$(date +"%Y-%m-%d_%H-%M-%S").png"
-import "$filename"
-xclip -selection clipboard -t image/png -i "$filename"

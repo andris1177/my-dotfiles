d9fadf79a2d41ccf97ca011f77b81b320b38d748
andris1177 <laszloandris1@gmail.com>
2025-01-15 19:49:45 +0100

    add config files

--- scripts/xorg/makeScreenshot.sh
+++ scripts/xorg/makeScreenshot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sleep 0.1
-screenshot_dir="$HOME/screenshots"
+screenshot_dir="/data/photos-videos/screenshots"
 filename="$screenshot_dir/screenshot_$(date +"%Y-%m-%d_%H-%M-%S").png"
 import "$filename"
 xclip -selection clipboard -t image/png -i "$filename"

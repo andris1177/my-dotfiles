2f381355e38129968542fcda22c1e74bf6cbc151
andris1177 <laszloandris1@gmail.com>
2025-01-15 19:27:59 +0100

    add config files

--- scripts/makeScreenshot.sh
+++ scripts/makeScreenshot.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
-
-# Directory to save screenshots
+sleep 0.1
 screenshot_dir="/data/photos-videos/screenshots"
-
-# Filename for the screenshot
 filename="$screenshot_dir/screenshot_$(date +"%Y-%m-%d_%H-%M-%S").png"
-
-# Use grim and slurp for region selection
-grim -g "$(slurp)" "$filename"
-
-# Copy the screenshot to clipboard
-wl-copy < "$filename"
+import "$filename"
+xclip -selection clipboard -t image/png -i "$filename"

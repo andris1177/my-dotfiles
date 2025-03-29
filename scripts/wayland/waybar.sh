6c7ebbf6cc791175eafeba65feef679a518ea589
andris1177 <laszloandris1@gmail.com>
2025-03-29 20:30:35 +0100

    update hyprland dotfiles

--- scripts/wayland/waybar.sh
+++ scripts/wayland/waybar.sh
@@ -1,2 +1,3 @@
 killall waybar
-waybar -c .config/waybar/config.json -s .config/waybar/style.css
+waybar -c .config/waybar/config-top.json -s .config/waybar/style.css
+waybar -c .config/waybar/config-bottom.json -s .config/waybar/style.css
